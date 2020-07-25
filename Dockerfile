35680dde5291c0ea57d6ff78caad9e833656c26b
NikitaYatsuraPrograming <NikitaYatsuraPrograming@gmail.com>
2020-07-25 22:07:32 +0300

    settings for herocu

--- Dockerfile
+++ Dockerfile
@@ -11,3 +11,5 @@ WORKDIR /usr/src/app
 COPY . .
 
 EXPOSE 8000
+
+ENV SECRET_KEY = "+6^1!pyr55exgnez0ci*&g^d@hbt9!lh@=(cdg-%5(d+^%5tx-", DJANGO_DEBUG = ''
--- api_board/settings.py
+++ api_board/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "+6^1!pyr55exgnez0ci*&g^d@hbt9!lh@=(cdg-%5(d+^%5tx-"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ["0.0.0.0"]
 
